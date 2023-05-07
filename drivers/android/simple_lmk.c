a856eb5523fbc18827d44446737ad914a164884b
Sultan Alsawaf <sultan@kerneltoast.com>
2023-05-07 10:33:04 -0700

    simple_lmk: Ratelimit the 'no processes available to kill' message
    Under extreme simulated memory pressure, the 'no processes available to
    kill' message can be spammed hundreds of thousands of times, which is not
    productive. Ratelimit it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -9,6 +9,7 @@
 #include <linux/mm.h>
 #include <linux/moduleparam.h>
 #include <linux/oom.h>
+#include <linux/ratelimit.h>
 #include <linux/sort.h>
 #include <linux/vmpressure.h>
 #include <uapi/linux/sched/types.h>
@@ -183,7 +184,7 @@ static void scan_and_kill(unsigned long pages_needed)
 
 	/* Pretty unlikely but it can happen */
 	if (unlikely(!nr_found)) {
-		pr_err("No processes available to kill!\n");
+		pr_err_ratelimited("No processes available to kill!\n");
 		return;
 	}
 
