77578ddd454d6710b47a92f532bf12ab35eb5e41
akari <akariondev@gmail.com>
2024-02-04 20:24:38 +0000

    Revert "[REAPPLY] kernel: Boost CPU & DDR bus for a short amount of time when zygote forks"
    This reverts commit 1189ad3fee2e2028ea620a2f858ddf72cd8d8ec7.

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,8 +95,6 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
-#include <linux/cpu_input_boost.h>
-
 #include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
@@ -2253,9 +2251,8 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current))
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+	if (task_is_zygote(current) && df_boost_within_input(1000))
+		devfreq_boost_kick_max(DEVFREQ_CPU_DDR_BW, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
