3bca0e293ac53443b5b076133699a7ac98111615
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-02-10 23:24:53 +0800

    kernel/sys.c: easter egg for kernelsu users :D

--- kernel/sys.c
+++ kernel/sys.c
@@ -1168,7 +1168,15 @@ static void override_custom_release(char __user *release, size_t len)
 		copy_to_user(release, CONFIG_UNAME_OVERRIDE_STRING,
 			       strlen(CONFIG_UNAME_OVERRIDE_STRING) + 1);
 	}
-
+#ifdef CONFIG_KSU	
+	if (strstr(buf, "me.weishu.kernelsu")) {
+		char easteregg[50];
+		strcpy(easteregg, UTS_RELEASE);
+		strcat(easteregg, " // path_umount: ✅");
+		copy_to_user(release, easteregg,
+			       strlen(easteregg) + 1);
+	}
+#endif	
 	kfree(buf);
 
 }
